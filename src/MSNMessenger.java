12dd9718c4c608345a162ed8e6cb446a2918a6b4
Geazi <oigeazi@gmail.com>
2024-06-19 09:33:28 -0300

    Herança

--- src/MSNMessenger.java
+++ src/MSNMessenger.java
@@ -1,25 +1,3 @@
-public class MSNMessenger {
-    public void enviarMensagem() {
-        // primeiro confirmar se esta conectado a internet
-        validarConectadoInternet();
+public class MSNMessenger extends ServicoMensagemInstantanea {
 
-        System.out.println("Enviando mensagem");
-
-        // depois de enviada, salva o histórico da mensagem
-        salvarHistoricoMensagem();
-
-    }
-
-    public void receberMensagem() {
-        System.out.println("Recebendo mensagem");
-    }
-
-    // métodos privadas, visíveis somente na classe
-    private void validarConectadoInternet() {
-        System.out.println("Validando se está conectado a internet");
-    }
-
-    private void salvarHistoricoMensagem() {
-        System.out.println("Salvando o histórico da mensagem");
-    }
 }
