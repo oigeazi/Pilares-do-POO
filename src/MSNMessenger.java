f3a0e5db6415871ebef34d0b2f1db5433f24b450
Geazi <oigeazi@gmail.com>
2024-06-19 09:27:22 -0300

    Encapsulamento

--- src/MSNMessenger.java
+++ src/MSNMessenger.java
@@ -1,17 +1,25 @@
 public class MSNMessenger {
     public void enviarMensagem() {
+        // primeiro confirmar se esta conectado a internet
+        validarConectadoInternet();
+
         System.out.println("Enviando mensagem");
+
+        // depois de enviada, salva o histórico da mensagem
+        salvarHistoricoMensagem();
+
     }
 
     public void receberMensagem() {
         System.out.println("Recebendo mensagem");
     }
 
-    public void validarConectadoInternet() {
+    // métodos privadas, visíveis somente na classe
+    private void validarConectadoInternet() {
         System.out.println("Validando se está conectado a internet");
     }
 
-    public void salvarHistoricoMensagem() {
+    private void salvarHistoricoMensagem() {
         System.out.println("Salvando o histórico da mensagem");
     }
 }
